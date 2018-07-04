2342e806ba79787d96d7cc583fffd298049b907c
ShubhamThakur11 <shubhamt426@gmail.com>
2018-07-04 15:23:11 +0530

    new error

--- app/src/main/java/pref/com/beerproject/MainActivity.java
+++ app/src/main/java/pref/com/beerproject/MainActivity.java
@@ -34,7 +34,7 @@ import java.util.Map;
 public class MainActivity extends AppCompatActivity {
         private RecyclerView recyclerView;
         public RecyclerView.Adapter adapter;
-        Button b;
+        Button b,b1;
         TextView tv;
         private List<ListItem> listItems;
         private RequestQueue mqueue;
